08466b87b6bbe24c2066bf1ff52e1448b15a5738
zhouxiaoshan <xiaoshan.zhou@bayesba.com>
2019-06-05 16:23:30 +0800

    describe_feature_type

--- bayes/wfs/http/__init__.py
+++ bayes/wfs/http/__init__.py
@@ -4,13 +4,16 @@ import logging
 
 from bayes.wfs.core import Configuration
 
+
 class NoWFSModuleFound(Exception):
     def __init__(self):
         super().__init__('No WFS Module Found in bayes.wfs.datasource')
 
+
 bp = bp()
 
-def load_wfs(cfg_path = None):
+
+def load_wfs(cfg_path=None):
     from bayes.wfs.core.loader import load_datasource
     wfs_modules = load_datasource()
     if not wfs_modules:
@@ -18,17 +21,18 @@ def load_wfs(cfg_path = None):
 
     cfg = Configuration(cfg_path)
 
-    bp.wfs = wfs_modules[-1](cfg) # use last. index by name may be better.
+    bp.wfs = wfs_modules[-1](cfg)  # use last. index by name may be better.
     logging.info(f'Find WFS Module {bp.wfs.version} {bp.wfs.service_identification}')
 
     bp.encoder = importlib.import_module(cfg.get('wfs.output_encoder', 'bayes.wfs.encoder.json'))
+
 
 @bp.get('/wfs/capabilities')
 def wfs():
     kwargs = {
         'accept_versions': [],
         'sections': ['ALL'],
-        'update_sequence': None, 
+        'update_sequence': None,
         'accept_formats': 'application/json'
     }
 
@@ -38,6 +42,14 @@ def wfs():
         kwargs['sections'] = request.GET.sections.split(',')
     if request.GET.accept_formats:
         kwargs['accept_formats'] = request.GET.accept_formats
-    
+
     caps = bp.wfs.get_capabilities(**kwargs)
     return bp.encoder.encode(caps)
+
+
+@bp.get('/wfs/describe_feature_type')
+def wfs():
+    table_name = request.GET.tablenames.split(',')
+
+    feature_types = bp.wfs.describe_feature_type(table_name)
+    return {feature_type: bp.encoder.encode(feature_types.get(feature_type)) for feature_type in feature_types}
